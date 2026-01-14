f6870f2cbe889c0809e3ad641afe93c811531f69
Henrik <602119@stud.hvl.no>
2026-01-14 15:22:32 +0100

    Camera Test Successful

--- UnityProjects/CERN_Aruco/Assets/Scripts/TagDetection.cs
+++ UnityProjects/CERN_Aruco/Assets/Scripts/TagDetection.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class TagDetection : MonoBehaviour
 {
+
+    [Header("Setup")] 
+    public GameObject box;
+    
+    private WebCamTexture webcamTexture;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        webcamTexture = new WebCamTexture(1920, 1080, 30);
+        webcamTexture.Play();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!webcamTexture.isPlaying || !webcamTexture.didUpdateThisFrame)
+            return;
+        
+        box.GetComponent<Renderer>().material.color = Color.forestGreen;
         
     }
 }
